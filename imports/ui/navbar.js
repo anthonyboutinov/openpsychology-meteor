c6aaaa16f7a03649971ff56809c065ba6049b5d7
Anthony Boutinov <anton4488@gmail.com>
2016-11-07 19:47:34 +0300

    Подготовка к аккаунтам

--- imports/ui/navbar.js
+++ imports/ui/navbar.js
@@ -1,7 +1,7 @@
 import { Categories } from '../api/categories.js';
 
 Template.navbar.helpers({
-  "searchbarActive": function() {
+  searchbarActive: function() {
     if (this.showSearchbar) {
       return "active";
     } else {
@@ -9,14 +9,18 @@ Template.navbar.helpers({
       if (cc == null) {
         return false;
       }
-      const ccValue = cc(); 
+      const ccValue = cc();
       return ccValue && ccValue.count() > 1 ? "searchbar-active-but-hidden" : false;
     }
   },
-  "isActiveRouteHome": function() {
+  isActiveRouteHome: function() {
     return Router.current().route.getName() == "home" ? "active" : false;
   },
-  "categories": function() {
+  categories: function() {
     return Categories.find({}, {sort: {order: 1}});
+  },
+
+  username: function() {
+    return Meteor.user().emails[0].address;
   },
 });
