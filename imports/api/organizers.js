d87458f65fd0e78e4ea0b77482837bde68f9b9a3
Anthony Boutinov <anton4488@gmail.com>
2016-11-13 02:12:02 +0300

    Organizers schema

--- imports/api/organizers.js
+++ imports/api/organizers.js
@@ -4,6 +4,58 @@ import { Categories } from './categories.js';
 
 export const Organizers = new Mongo.Collection("organizers");
 
+const ManagedBy = new SimpleSchema({
+  userId: {
+    type: String,
+  },
+  nonRetireable: {
+    type: Boolean,
+    label: "Обладатель объекта Организация",
+    optional: true
+  }
+});
+
+Organizers.attachSchema(new SimpleSchema({
+  name: {
+    type: String,
+    label: "Название",
+    max: 100,
+    min: 3
+  },
+  description: {
+    type: 'markdown',
+    label: "Описание",
+    max: 2000,
+    optional: true
+  },
+  bannerUrl: {
+    type: String,
+    label: "Изображение-баннер",
+    optional: true
+  },
+  imageUrl: {
+    type: String,
+    label: "Логотип",
+    optional: true
+  },
+  phone: {
+    type: Number,
+    label: "Номер телефона",
+    min: 6,
+    max: 10,
+    optional: true,
+  },
+  location: {
+    type: String,
+    label: "Адрес",
+    optional: true
+  },
+  managedBy: {
+    type: [ManagedBy],
+    label: "Управляющие"
+  }
+}));
+
 
 if (Meteor.isServer) {
 
