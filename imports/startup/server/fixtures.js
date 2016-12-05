616a8952de55659f83fc6da6757858437254719b
Anthony Boutinov <anton4488@gmail.com>
2016-12-05 21:38:05 +0300

    Structurize Project: imports/startup/server/placeholderdata

--- imports/startup/server/fixtures.js
+++ imports/startup/server/fixtures.js
@@ -1,8 +1,62 @@
-const hasAdmin = Meteor.users.findOne( { 'emails.address': 'anton4488@gmail.com' } );
+Meteor.startup(() => {
 
-if ( !hasAdmin ) {
-  Accounts.createUser({
-    email: 'anton4488@gmail.com',
-    password: 'password'
-  });
-}
+  import { Categories }  from '../../api/categories/index.js';
+
+
+
+  /*
+   * Set up admin accounts
+   */
+
+  const hasAdmin = Meteor.users.findOne( { 'emails.address': 'anton4488@gmail.com' } );
+
+  if ( !hasAdmin ) {
+    Accounts.createUser({
+      email: 'anton4488@gmail.com',
+      password: 'password'
+    });
+  }
+
+
+  /*
+   * Set up Categories
+   */
+
+  if (Categories.find().count() == 0) {
+
+    const categories = [
+      {
+        urlName: "t",
+        pluralName: "Тренинги",
+        singularName: "Тренинг",
+        order: 1,
+      },
+      {
+        urlName: "s",
+        pluralName: "Семинары",
+        singularName: "Семинар",
+        order: 2,
+      },
+      {
+        urlName: "e",
+        pluralName: "Образование",
+        singularName: "Образование",
+        order: 3,
+      },
+      {
+        urlName: "o",
+        pluralName: "Мероприятия онлайн",
+        singularName: "Онлайн мероприятие",
+        order: 4,
+      },
+    ];
+
+    categories.forEach((category) => {
+      Categories.insert(category);
+    });
+
+    console.log(Categories.find().count() + " categories in total.");
+
+  };
+
+});
