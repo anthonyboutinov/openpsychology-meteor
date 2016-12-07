7d164ba13ef2f30a2e97a5ae59214d1ffa4256b5
Anthony Boutinov <anton4488@gmail.com>
2016-12-07 19:08:17 +0300

    Contenteditable reactive dict

--- imports/ui/components/common/contenteditable/display.js
+++ imports/ui/components/common/contenteditable/display.js
@@ -1,1 +1,20 @@
 import './display.html';
+
+import { ReactiveDict } from 'meteor/reactive-dict';
+
+Template.contenteditable.onCreated(function() {
+  this.data.state = new ReactiveDict();
+  this.data.state.set('storedValue', false);
+});
+
+Template.contenteditable.helpers({
+  displayValue: function() {
+    return this.state.get("storedValue") ? false : this.value;
+  },
+});
+
+Template.contenteditable.events({
+  "dblclick [data-conteneditable='true']": function(event, template) {
+    template.data.state.set('storedValue', this.value);
+  },
+});
--- imports/ui/components/common/contenteditable/update.js
+++ imports/ui/components/common/contenteditable/update.js
@@ -2,38 +2,54 @@ import './update.html';
 
 import * as selectAllText from '/imports/ui/selectAllText.js';
 
+function makeCall(template) {
+  const target = $("[data-id='contenteditableField']");
+  const val = target.text();
+  if (val && val != template.data.placeholder) {
+    Meteor.call(template.data.meteorCallName, val, function(error, result) {
+      console.log(error, result);
+      if (result) {
+        template.data.state.set('storedValue', false);
+        console.log(Meteor.user().profile.name);
+      }
+    });
+  }
+}
+
 Template.contenteditableUpdate.helpers({
+  valueOrPlaceholder() {
+    return this.value ? this.value : this.placeholder;
+  },
 });
 
 Template.contenteditableUpdate.events({
-  "keyup #contenteditableField": function(event, template) {
+  "keydown [data-id='contenteditableField']": function(event, template) {
     if (event.keyCode != 13) return;
-    const target = $(event.target);
-    target.text(target.html().replace("<br>", ""));
+    // const target = $(event.target);
+    event.preventDefault();
+    makeCall(template);
+    // target.text(target.html().replace("<br>", ""));
   },
-  "blur #contenteditableField": function(event, template) {
+
+  "blur [data-id='contenteditableField']": function(event, template) {
     const target = $(event.target);
     const val = target.text();
-    if (val != "" && val != template.data.placeholder) {
-      $("#contenteditableConfirmButton").removeClass("hidden");
+    if (val != "" && val != template.data.placeholder && val != template.data.state.get('storedValue')) {
+      $("[data-id='contenteditableConfirmButton']").removeClass("hidden");
     } else {
-      $("#contenteditableConfirmButton").addClass("hidden");
+      $("[data-id='contenteditableConfirmButton']").addClass("hidden");
+      template.data.state.set('storedValue', false);
     }
   },
-  "click #contenteditableConfirmButton": function(event, template) {
-    const target = $("#contenteditableField");
-    const val = target.text();
-    if (val != "" && val != template.data.placeholder) {
-      Meteor.call(template.data.meteorCallName, val, function(error, result) {
-        console.log(error, result);
-      });
-    }
+
+  "click [data-id='contenteditableConfirmButton']": function(event, template) {
+    makeCall(template);
   },
 });
 
 Template.contenteditableUpdate.onRendered(function() {
 
-  $("#contenteditableField").on("focus", function(event) {
+  $("[data-id='contenteditableField']").on("focus", function(event) {
     selectAllText._(event.target);
   }).focus();
 
