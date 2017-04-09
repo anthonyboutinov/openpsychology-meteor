4dcc93c1da58f4f822cb4af63c5fe71b778bdead
Anthony Boutinov <anton4488@gmail.com>
2017-04-10 00:14:05 +0300

    Comments

--- imports/api/coaches/hooks.js
+++ imports/api/coaches/hooks.js
@@ -3,3 +3,5 @@ import { Coaches } from './collection.js';
 Coaches.after.remove(function (userId, doc) {
   Images.remove({_id: doc.profilePicId});
 });
+
+// TODO: check if images are automatically removed when updating profile pircure
