bc084c15e8c2e4039e6614a6742823d681ced096
Anthony Boutinov <anton4488@gmail.com>
2017-06-29 00:29:54 +0300

    Roles, admin basic routing and set up

---
+++ imports/startup/server/fixtures/markdowns.js
@@ -0,0 +1,25 @@
+import { Markdowns }  from '/imports/api/markdowns/index.js';
+
+
+/*
+ * Set up Markdowns
+ */
+
+if (Markdowns.find().count() == 0) {
+
+  const markdowns = [
+    {
+      name: "terms",
+      title: "Условия пользования",
+      description: "[Описание для результатов поисковых запросов]",
+      data: Assets.getText("terms.md"),
+    },
+  ];
+
+  markdowns.forEach((m) => {
+    Markdowns.insert(m);
+  });
+
+  console.log(Markdowns.find().count() + " markdowns in total.");
+
+};
