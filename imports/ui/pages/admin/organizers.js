2043206fd2b4b7b3b4ea77f000a7311b77f0fdb1
Anthony Boutinov <anton4488@gmail.com>
2017-06-30 23:40:09 +0300

    Upd formatUserFn, reactive-table.scss

--- imports/ui/pages/admin/organizers.js
+++ imports/ui/pages/admin/organizers.js
@@ -7,13 +7,16 @@ Template.adminOrganizers.helpers({
   settings() {
     return _.extend({
       collection: Organizers,
-      // fields: [
-      //   { key: 'name', label: 'Название' },
-      //   { key: 'refKind', label: 'Тип объектов', fn: formatObjectFn },
-      //   { key: 'maxItems', label: 'Макс. вместимость', fn: formatObjectFn },
-      //   { key: 'updatedAt', label: 'Обновлено', fn: formatDateFn },
-      //   { key: 'updatedBy', label: 'Обновлено кем', fn: formatUserFn },
-      // ]
+      fields: [
+        { key: 'name', label: 'Название' },
+        { key: 'location', label: 'Местположение', fn: formatObjectFn },
+        { key: 'ownerId', label: 'Владелец', fn: formatUserFn },
+        { key: 'managedBy', label: 'Управляющие', fn: formatUserFn },
+        { key: 'createdAt', label: 'Создано', fn: formatDateFn },
+        { key: 'createdBy', label: 'Создано кем', fn: formatUserFn },
+        { key: 'updatedAt', label: 'Обновлено', fn: formatDateFn },
+        { key: 'updatedBy', label: 'Обновлено кем', fn: formatUserFn },
+      ]
     }, defaultReactiveTableSettings)
   },
 });
@@ -21,6 +24,6 @@ Template.adminOrganizers.helpers({
 Template.adminOrganizers.events({
   'dblclick .reactive-table tbody tr': function (event) {
     var doc = this;
-    Router.go('admin.editAny', {collectionName: 'organizers', _id: doc._id, prevPageTitle: "Организаторы"});
+    Router.go('admin.editAny', {collectionName: 'organizers', _id: doc._id, prevPageTitle: "Организации"});
   }
 });
