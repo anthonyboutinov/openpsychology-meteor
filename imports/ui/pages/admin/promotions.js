9b5f5767721f2d66d0adb34b704b1f082bb8ea98
Anthony Boutinov <anton4488@gmail.com>
2017-06-30 14:15:39 +0300

    reactiveTable

--- imports/ui/pages/admin/promotions.js
+++ imports/ui/pages/admin/promotions.js
@@ -1,1 +1,23 @@
 import './promotions.html';
+
+import { Groups } from '/imports/api/groups/collection.js';
+
+Template.adminPromotions.helpers({
+  settings() {
+    return {
+      collection: Groups,
+      useFontAwesome:true,
+      rowsPerPage:30,
+      showRowCount:true,
+      showColumnToggles:true,
+      enableRegex:true,
+    }
+  },
+});
+
+Template.adminPromotions.events({
+  'dblclick .reactive-table tbody tr': function (event) {
+    var row = this;
+    console.log(row);
+  }
+});
