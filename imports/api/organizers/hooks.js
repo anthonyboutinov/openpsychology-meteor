048f4711a7b099ce3c91c5e9e45aa9b2dbebc5a3
Anthony Boutinov <anton4488@gmail.com>
2017-07-01 21:04:35 +0300

    Security Permissions: groups, users

--- imports/api/organizers/hooks.js
+++ imports/api/organizers/hooks.js
@@ -1,10 +1,18 @@
 import { Organizers } from './collection.js';
 import { Events } from '/imports/api/events/collection.js';
 import { Coaches } from '/imports/api/coaches/collection.js';
-
+import { SystemNotifications } from '/imports/api/systemNotifications.js';
 
 Organizers.after.remove(function (userId, doc) {
   Coaches.remove({organizerId: doc._id});
   Events.remove({organizerId: doc._id});
   UserFiles.remove({_id: {$in: [doc.bannerImageId, doc.imageId]}});
+});
+
+Organizers.after.insert(function (userId, doc) {
+  const user = Meteor.user();
+  if (_.contains(user.appSpecific.systemNotifications, SystemNotifications.showWelcomeNotification)) {
+    const modifier = {$pull: {"appSpecific.systemNotifications": SystemNotifications.showWelcomeNotification}};
+    Meteor.users.update(userId, modifier);
+  }
 });
--- imports/api/users/hooks.js
+++ imports/api/users/hooks.js
@@ -4,22 +4,22 @@ Meteor.users.after.remove(function (userId, doc) {
   Organizers.remove({ownerId: userId});
 }, {fetchPrevious: false});
 
-Meteor.users.before.update(function (userId, doc, fieldNames, modifier, options) {
-
-
-  console.log(userId, doc, fieldNames, modifier, options);
-  //TODO: Safety checks!
-  // // SAFETY CHECKS FOR SUPER ADMIN
-  // if (_.indexOf(fieldNames, "userProfile.permissions"
-  // const superaAmin = Meteor.users.findOne({ 'emails.address': 'anton4488@gmail.com' });
-  // if (userId == superaAmin._id) {
-  //   // ABORT ANY PERMISSION CHANGES
-  //
-  //   modifier.$set.userProfile.permissions =
-  // }
-}, {fetchPrevious: false});
+// Meteor.users.before.update(function (userId, doc, fieldNames, modifier, options) {
+//
+// }, {fetchPrevious: false});
 
 
 Meteor.users.after.update(function (userId, doc, fieldNames, modifier, options) {
-  console.log("----------Meteor.users.after.update--------------\n\n", userId, doc, fieldNames, modifier, options);
+  console.log("\n\n----------Meteor.users.after.update--------------\n", {userId, fieldNames, modifier, options});
+
+  // TODO: rm showConfirmEmailNotification after email gets verified
+  // const setModifier = modifier['$set'];
+  // if (setModifier) {
+  //
+  // }
+  // const user = Meteor.user();
+  // if (_.contains(user.appSpecific.systemNotifications, SystemNotifications.showConfirmEmailNotification)) {
+  //   const modifier = {$pull: {"appSpecific.systemNotifications": SystemNotifications.showConfirmEmailNotification}};
+  //   Meteor.users.update(userId, modifier);
+  // }
 }, {fetchPrevious: false});
