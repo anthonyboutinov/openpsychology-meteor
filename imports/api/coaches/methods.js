4b3166c3f5cb1a6a474164738cf030fce9db7cbe
Anthony Boutinov <anton4488@gmail.com>
2017-07-04 02:44:37 +0300

    Coaches security, forse-ssl

--- imports/api/coaches/methods.js
+++ imports/api/coaches/methods.js
@@ -1,39 +1,21 @@
-if (Meteor.isServer) {
-
   import { check } from 'meteor/check';
   import { Coaches } from './collection.js';
   import { Organizers } from '/imports/api/organizers/collection.js';
 
   Meteor.methods({
 
+    // @secure
     'coach.remove': function(_id) {
       check(_id, String);
-      check(this.userId, String);
-      const managedOrganizers = Organizers.find(
-        {
-          $or: [
-            {ownerId: this.userId},
-            {managedBy: this.userId}
-          ]
-        },
-        {fields: {_id: 1}}).map(function(doc){return doc._id});
-      return Coaches.remove({_id: _id, organizerId: {$in: managedOrganizers}});
+      Security.can(this.userId).remove(_id).for(Coaches).throw();
+      return Coaches.remove(_id);
     },
 
+    // @secure
     'coaches.remove': function(coachesIds) {
       check(coachesIds, Array);
-      check(this.userId, String);
-      const managedOrganizers = Organizers.find(
-        {
-          $or: [
-            {ownerId: this.userId},
-            {managedBy: this.userId}
-          ]
-        },
-        {fields: {_id: 1}}).map(function(doc){return doc._id});
-      return Coaches.remove({_id: {$in: coachesIds}, organizerId: {$in: managedOrganizers}});
+      Security.can(this.userId).remove(_id).for(Coaches).throw();
+      return Coaches.remove({_id: {$in: coachesIds}});
     },
 
   });
-
-}
