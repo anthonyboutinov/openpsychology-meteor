098d311f76672b9b9b0321c2a514c1be0b80568a
Anthony Boutinov <anton4488@gmail.com>
2017-07-05 03:01:36 +0300

    Fix errors and logic in organizers.update hooks

--- imports/ui/pages/dashboard/organizers/update.js
+++ imports/ui/pages/dashboard/organizers/update.js
@@ -19,7 +19,7 @@ let hooksObject = {
       }
       if (doc.$set && doc.$set.ownerId && doc.$set.ownerId != this.currentDoc.ownerId) {
         // If we set new owner to be someone from the list of managedBy users, then remove this id from the list
-        if (this.currentDoc.managedBy.includes(doc.$set.ownerId)) {
+        if (this.currentDoc.managedBy && this.currentDoc.managedBy.includes(doc.$set.ownerId)) {
           if (doc.$set.managedBy === undefined) {
             // If there are no changes to the list of managedBy users,
             // Initialize it but without this new ownerId
@@ -32,10 +32,17 @@ let hooksObject = {
 
         // Add old ownerId to managedBy users list
         //
+        console.log({mb1: doc.$set.managedBy});
         if (doc.$set.managedBy === undefined) {
           doc.$set.managedBy = this.currentDoc.managedBy;
         }
+        console.log({mb2: doc.$set.managedBy});
+        if (!doc.$set.managedBy) {
+          doc.$set.managedBy = [];
+        }
+        console.log({mb3: doc.$set.managedBy});
         doc.$set.managedBy.push(this.currentDoc.ownerId);
+        console.log({mb4: doc.$set.managedBy});
       }
       console.log("after: ", doc);
       return doc;
@@ -44,14 +51,26 @@ let hooksObject = {
   onSuccess(formType, result) {
     console.log(this);
 
-    if (this.updateDoc.$set && this.updateDoc.$set.ownerId && this.updateDoc.$set.ownerId !=this.currentDoc.ownerId) {
-      swal("Успешно!", "Права на огранизацию «" + this.currentDoc.name + "» переданы другому пользователю!", "success");
+    const didChangeOwner = this.updateDoc.$set && this.updateDoc.$set.ownerId;
+    if (didChangeOwner && this.updateDoc.$set.ownerId != this.currentDoc.ownerId) {
+      swal("Успешно!", "Права на огранизацию «" + this.currentDoc.name + "» переданы другому пользователю! Вы все еще являеесь участником. Если вы хотите покинуть организацию, удалите себя из списка участников.", "success");
     }
 
-    Router.go('dashboard.organizer', {_id: this.formAttributes.doc._id});
+    if (didChangeOwner) {
+      // TODO: This commented out line doesn't work right now for some reason.
+      // Router.go('dashboard.organizer.update', {_id: this.formAttributes.doc._id, specialFormType: 'collaborators'});
+      Router.go('dashboard.organizer', {_id: this.formAttributes.doc._id});
+    } else {
+      if (!this.updateDoc.$set.managedBy /*if did not update managers*/) {
+        // Means performed ordinary update, so we go back
+        Router.go('dashboard.organizer', {_id: this.formAttributes.doc._id});
+      }
+    }
+
+
   },
   onError(formType, error) {
-    console.log(error);
+    swal("Ошибка!", error, danger);
   },
 };
 AutoForm.addHooks(['updateOrganizerForm'], hooksObject);
